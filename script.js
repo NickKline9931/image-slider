b19741881b86f37944b32b92b1b760c1bcce6033
Nick <nickkline9931@gmail.com>
2022-09-08 16:09:10 -0400

    Add back button functionality

--- script.js
+++ script.js
@@ -20,19 +20,36 @@ function displayFive() {
     photo.src = "face5.svg";
 }
 
+function lastImage() {
+    if (photo.getAttribute('src') == "face5.svg") {
+        displayFour();
+    } else if (photo.getAttribute('src') == "face4.svg") {
+        displayThree();
+    } else if (photo.getAttribute('src') == "face3.svg") {
+        displayTwo();
+    } else if (photo.getAttribute('src') == "face2.svg") {
+        displayOne();
+    } else if (photo.getAttribute('src') == "face1.svg") {
+        displayFive();
+}
+}
+
 function nextImage() {
     if (photo.getAttribute('src') == "face1.svg") {
-        photo.src = "face2.svg";
+        displayTwo();
     } else if (photo.getAttribute('src') == "face2.svg") {
-        photo.src = "face3.svg";
+        displayThree();
     } else if (photo.getAttribute('src') == "face3.svg") {
-        photo.src = "face4.svg";
+        displayFour();
     } else if (photo.getAttribute('src') == "face4.svg") {
-        photo.src = "face5.svg";
+        displayFive();
     } else if (photo.getAttribute('src') == "face5.svg") {
-        photo.src = "face1.svg";
+        displayOne();
 }
 }
+
+let backButton = document.getElementById("backbutton");
+backButton.addEventListener("click", lastImage);
 
 let nextButton = document.getElementById("nextbutton");
 nextButton.addEventListener("click", nextImage);
